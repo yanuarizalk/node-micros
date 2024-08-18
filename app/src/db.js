2f036e030c1dd6ef6b2a614ed08d56eb47317b01
yanuarizalk <me@yanuarizal.net>
2024-08-18 18:27:35 +0700

    Configurable db host

--- app/src/db.js
+++ app/src/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize({
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.DB_HOST ?? 'localhost',
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
